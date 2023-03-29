6b98b85dfea0566605658f4ea87518f9e9845b14
Marco Concetto Rudilosso <marco.rudilosso@shopify.com>
2023-03-29 13:46:16 +0200

    use grpcio

--- build.rs
+++ build.rs
@@ -1,10 +1,19 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(false)
-        .compile(
-            &["googleapis/google/storage/v2/storage.proto"],
-            &["googleapis"],
-        )?;
+    let proto_root = "googleapis";
+    println!("cargo:rerun-if-changed={}", proto_root);
+    protoc_grpcio::compile_grpc_protos(
+        &[
+            "google/storage/v2/storage.proto",
+            "google/type/date.proto",
+            "google/iam/v1/policy.proto",
+            "google/iam/v1/iam_policy.proto",
+            "google/iam/v1/options.proto",
+            "google/type/expr.proto",
+        ],
+        &[proto_root],
+        &"src/protos",
+        None,
+    )
+    .expect("Failed to compile gRPC definitions!");
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,119 +1,89 @@
-use gcp_auth::AuthenticationManager;
-use gcp_auth::Token;
-use google::storage::v2::storage_client::StorageClient;
-use google::storage::v2::ListObjectsRequest;
-use google::storage::v2::Object;
-use tokio::io::AsyncRead;
-use tokio_stream::StreamExt;
-use tokio_util::io::StreamReader;
-use tonic::transport::Channel;
-use tonic::transport::Endpoint;
-use tonic::Request;
+use std::sync::Arc;
 
-pub mod google {
-    pub mod storage {
-        pub mod v2 {
-            tonic::include_proto!("google.storage.v2");
-        }
-    }
-    pub mod iam {
-        pub mod v1 {
-            tonic::include_proto!("google.iam.v1");
-        }
-    }
+use grpcio::{ChannelBuilder, ChannelCredentials, EnvBuilder};
 
-    pub mod r#type {
-        tonic::include_proto!("google.r#type");
-    }
-}
+mod protos;
 
 #[derive(Clone)]
 pub struct GcsClient {
-    client: StorageClient<Channel>,
-    token: Token,
+    client: protos::storage_grpc::StorageClient,
 }
 
-struct GrpcError {
-    code: tonic::Code,
-}
-
-impl From<tonic::Status> for GrpcError {
-    fn from(status: tonic::Status) -> Self {
-        Self {
-            code: status.code(),
-        }
-    }
-}
-
-impl From<GrpcError> for std::io::Error {
-    fn from(error: GrpcError) -> Self {
-        match error.code {
-            tonic::Code::NotFound => std::io::ErrorKind::NotFound,
-            tonic::Code::PermissionDenied => std::io::ErrorKind::PermissionDenied,
-            _ => std::io::ErrorKind::Other,
-        }
-        .into()
-    }
-}
-
+const URL: &str = "storage.googleapis.com";
 
 impl GcsClient {
-    pub async fn new<T: Into<Endpoint>>(endpoint: T) -> Result<Self, anyhow::Error> {
-        let authentication_manager = AuthenticationManager::new().await?;
-        let scopes = &["https://www.googleapis.com/auth/devstorage.read_only"];
-        let token = authentication_manager.get_token(scopes).await?;
+    pub fn new() -> Result<Self, anyhow::Error> {
+        let env = Arc::new(EnvBuilder::new().build());
+        let credentials = ChannelCredentials::google_default_credentials()?;
+        let channel = ChannelBuilder::new(env)
+            .set_credentials(credentials)
+            .connect(URL);
         Ok(Self {
-            client: StorageClient::connect(endpoint).await?,
-            token,
+            client: protos::storage_grpc::StorageClient::new(channel),
         })
     }
 
-    pub async fn list_objects(
+    pub fn list_objects(
         &mut self,
         bucket: &str,
         prefix: &str,
-    ) -> Result<Vec<Object>, tonic::Status> {
-        let request = tonic::Request::new(ListObjectsRequest {
+    ) -> grpcio::Result<Vec<protos::storage::Object>> {
+        let request = protos::storage::ListObjectsRequest {
             parent: bucket.to_string(),
             prefix: prefix.to_string(),
             ..Default::default()
-        });
+        };
 
-        let request = self.add_auth_to_request(request);
-
-        let response = self.client.list_objects(request).await?;
-        Ok(response.into_inner().objects)
+        let response = self.client.list_objects(&request)?;
+        Ok(response.objects.to_vec())
     }
 
-    pub async fn read_object(
-        &mut self,
-        bucket: &str,
-        object: &str,
-    ) -> Result<impl AsyncRead, tonic::Status> {
-        let request = tonic::Request::new(google::storage::v2::ReadObjectRequest {
-            bucket: bucket.to_string(),
-            object: object.to_string(),
-            ..Default::default()
-        });
+    // pub fn list_objects(
+    //     &mut self,
+    //     bucket: &str,
+    //     prefix: &str,
+    // ) -> Result<Vec<Object>, tonic::Status> {
+    //     let request = tonic::Request::new(ListObjectsRequest {
+    //         parent: bucket.to_string(),
+    //         prefix: prefix.to_string(),
+    //         ..Default::default()
+    //     });
 
-        let request = self.add_auth_to_request(request);
+    //     let request = self.add_auth_to_request(request);
 
-        let response = self.client.read_object(request).await?;
-        let response = response.into_inner();
-        Ok(StreamReader::new(response.filter_map(
-            |chunk| match chunk {
-                Ok(chunk) => match chunk.checksummed_data {
-                    Some(data) => Some(Ok(std::io::Cursor::new(data.content))),
-                    None => None,
-                },
-                Err(e) => Some(Err(std::io::Error::from(GrpcError::from(e)))),
-            },
-        )))
-    }
+    //     let response = self.client.list_objects(request).await?;
+    //     Ok(response.into_inner().objects)
+    // }
 
-    fn add_auth_to_request<T>(&self, mut req: Request<T>) -> Request<T> {
-        req.metadata_mut()
-            .insert("Authorization", self.token.as_str().parse().unwrap());
-        req
-    }
+    // pub async fn read_object(
+    //     &mut self,
+    //     bucket: &str,
+    //     object: &str,
+    // ) -> Result<impl AsyncRead, tonic::Status> {
+    //     let request = tonic::Request::new(google::storage::v2::ReadObjectRequest {
+    //         bucket: bucket.to_string(),
+    //         object: object.to_string(),
+    //         ..Default::default()
+    //     });
+
+    //     let request = self.add_auth_to_request(request);
+
+    //     let response = self.client.read_object(request).await?;
+    //     let response = response.into_inner();
+    //     Ok(StreamReader::new(response.filter_map(
+    //         |chunk| match chunk {
+    //             Ok(chunk) => match chunk.checksummed_data {
+    //                 Some(data) => Some(Ok(std::io::Cursor::new(data.content))),
+    //                 None => None,
+    //             },
+    //             Err(e) => Some(Err(std::io::Error::from(GrpcError::from(e)))),
+    //         },
+    //     )))
+    // }
+
+    // fn add_auth_to_request<T>(&self, mut req: Request<T>) -> Request<T> {
+    //     req.metadata_mut()
+    //         .insert("Authorization", self.token.as_str().parse().unwrap());
+    //     req
+    // }
 }
